30835bc7f6d99eeea19459adcb2487519d218d11
HidehikoKondo <kondo@wonderpla.net>
2016-04-08 15:13:18 +0900

    スライダーで設定した値で検索

--- www/view.js
+++ www/view.js
@@ -2,10 +2,29 @@ $(function(){
     //ローディング隠す
     $("#progress_bar").hide();
 
-    //ボタンのクラス適用
+    //クラス未設定のボタンに共通のcssクラスを適用
     $("button").each(function(){
         if($(this).hasClass("")){
             $(this).addClass("mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent");
         }
     });
 });
+
+//スライダー
+function change_slider(){
+    console.log($("#bust_slider").val());
+    $("#bustsize").val($("#bust_slider").val());
+    change_bustsize();
+}
+
+//バストサイズの入力form
+function change_bustsize(){
+    bustsize = $("#bustsize").val();
+    clear_page();
+    request(1, bustsize);
+}
+
+//商品一覧を消す
+function clear_page(){
+    $('#products_list').text("");
+}
